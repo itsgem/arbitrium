ab70b0eb753fbcdb23e530aee2e292bc991f6a4e
Donna Borja <dobidobz@gmail.com>
2016-04-18 15:05:58 +0800

    Setup swaggervel API docs with 2 sample endpoints (#1)
    * Setup swaggervel API docs with 2 sample endpoints
    Commands needed to run:
    `composer update`
    
    * changed contact email for api to info@arbitriumgroup.com

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -6,6 +6,28 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
+/**
+ * Class Controller
+ *
+ * @SWG\Swagger(
+ *     schemes={"http", "https"},
+ *     basePath="/api/v1",
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="Arbitrium",
+ *         description="A project by Arbitrium Group on decisions technology that helps make better decisions, such as financial, retail, pricing decisions, with data sets you already have.",
+ *         @SWG\Contact(
+ *             email="info@arbitriumgroup.com"
+ *         )
+ *     ),
+ *     @SWG\ExternalDocumentation(
+ *         description="Arbitrium GitHub",
+ *         url="https://github.com/nerubia/arbitrium"
+ *     )
+ * )
+ *
+ * @package App\Http\Controllers
+ */
 abstract class Controller extends BaseController
 {
     use DispatchesJobs, ValidatesRequests;
