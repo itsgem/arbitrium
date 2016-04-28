eac7c59fd555f08abd786dba805311b94a2381c0
dobidobz <dobidobz@gmail.com>
2016-04-28 11:29:34 +0800

    client reset email activation

--- reduxion/client/src/app/client/profile/containers/profile.js
+++ reduxion/client/src/app/client/profile/containers/profile.js
@@ -1,21 +1,27 @@
 import { connect } from 'react-redux';
-import { clientProfile, updateClientProfile, getAvailableUsername } from '../reducers/profile';
+import { clientProfile, updateClientProfile, getAvailableUsername, retrieveEmailChangeToken, verifyEmailChange } from '../reducers/profile';
 import { country } from '../../../parts/auth/reducers/country';
 import ClientProfile from '../views/profile';
 
 const mapStateToProps = (state) => {
-    return {
-        countryList: state.get('country').get('countryList'),
-        user: state.get('clientProfile').get('user'),
-        isUsernameAvailable: state.get('clientProfile').get('isUsernameAvailable'),
-        success: state.get('clientProfile').get('success'),
-        errors: state.get('clientProfile').get('errors'),
-    };
+  return {
+    countryList: state.get('country').get('countryList'),
+    user: state.get('clientProfile').get('user'),
+    isUsernameAvailable: state.get('clientProfile').get('isUsernameAvailable'),
+    isRetrieveEmailChangeTokenSuccess: state.get('clientProfile').get('isRetrieveEmailChangeTokenSuccess'),
+    isVerifyEmailChangeSuccess: state.get('clientProfile').get('isVerifyEmailChangeSuccess'),
+    emailChangeToken: state.get('clientProfile').get('emailChangeToken'),
+    loading: state.get('clientProfile').get('loading'),
+    success: state.get('clientProfile').get('success'),
+    errors: state.get('clientProfile').get('errors')
+  };
 }
 
 export default connect(mapStateToProps, {
-    country,
-    clientProfile,
-    updateClientProfile,
-    getAvailableUsername
+  country,
+  clientProfile,
+  updateClientProfile,
+  getAvailableUsername,
+  verifyEmailChange,
+  retrieveEmailChangeToken
 })(ClientProfile)
