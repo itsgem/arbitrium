5520b1932c12af82e604d257485c941f0f2272c6
dobidobz <dobidobz@gmail.com>
2016-04-29 09:54:57 +0800

    [admin] temporarily make main-menu as links instead of tabs to fix the unclickable issue

--- reduxion/admin/src/app/parts/admin/views/header.js
+++ reduxion/admin/src/app/parts/admin/views/header.js
@@ -10,24 +10,24 @@ export default React.createClass( {
             <div className="logo">
               <img src="https://s3.amazonaws.com/assets.idearobin.com/arbitrium/logo-arbitrium.png"></img>
             </div>
-            <div className="mdl-tabs mdl-js-tabs mdl-js-ripple-effect nav">
-              <div className="mdl-tabs__tab-bar tab">
-                <Link className="mdl-tabs__tab" to="/coffee">Dashboard</Link>
-                <Link className="mdl-tabs__tab" to="/coffee/client">Clients</Link>
-                <Link className="mdl-tabs__tab" to="/coffee/api">API</Link>
-                <Link className="mdl-tabs__tab" to="/coffee/pricing">Pricing</Link>
-                <Link className="mdl-tabs__tab" to="/coffee/account">Admin Accounts</Link>
-                <Link className="mdl-tabs__tab" to="/coffee/log">Logs</Link>
+            <div className="nav">
+              <nav className="mdl-navigation">
+                <Link className="mdl-navigation__link" to="/coffee">Dashboard</Link>
+                <Link className="mdl-navigation__link" to="/coffee/client">Clients</Link>
+                <Link className="mdl-navigation__link" to="/coffee/api">API</Link>
+                <Link className="mdl-navigation__link" to="/coffee/pricing">Pricing</Link>
+                <Link className="mdl-navigation__link" to="/coffee/account">Admin Accounts</Link>
+                <Link className="mdl-navigation__link" to="/coffee/log">Logs</Link>
                 <div className="icon-profile">
-                  <button id="menu" className="mdl-button mdl-js-button mdl-button--icon">
+                  <button id="menu" className="mdl-button mdl-js-button mdl-button--icon" data-upgraded=",MaterialButton">
                     <i className="material-icons">account_circle</i>
                   </button>
                   <ul className="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" htmlFor="menu">
-                    <li className="mdl-menu__item"><Link className="logout-text" to ="/coffee/setting">Settings</Link></li>
-                    <li className="mdl-menu__item"><Link className="logout-text" to ="/coffee/logout">Logout</Link></li>
+                    <li className="mdl-menu__item"><Link className="mdl-navigation__link" to ="/coffee/setting">Settings</Link></li>
+                    <li className="mdl-menu__item"><Link className="mdl-navigation__link" to ="/coffee/logout">Logout</Link></li>
                   </ul>
                 </div>
-              </div>
+              </nav>
             </div>
           </div>
         </div>
