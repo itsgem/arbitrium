f4d06bbb6761537d93a9bad84050ceabf46308cc
julian-idearobin <juliansuringa@yahoo.com>
2016-05-02 18:20:22 +0800

    add root reducer

--- reduxion/admin/src/app/redux/rootReducer.js
+++ reduxion/admin/src/app/redux/rootReducer.js
@@ -3,11 +3,8 @@ import { combineReducers } from 'redux-immutable';
 import auth from '../parts/auth/reducers/auth'
 import signup from '../parts/auth/reducers/signup'
 import profile from '../parts/profile/reducers/profile'
-import router from '../parts/core/reducers/routerRedux'
-//import country from '../parts/auth/reducers/country'
-
+import router from '../parts/auth/reducers/routerRedux'
 import adminUserManagement from '../parts/admin/reducers/userManagement'
-//import validateUsername from '../parts/admin/reducers/validateUsername'
 import clientadmin from '../parts/admin/reducers/clientProfile'
 
 export default combineReducers({
