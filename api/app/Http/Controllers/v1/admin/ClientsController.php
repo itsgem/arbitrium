8a599c3aa18bec358a95a591dc872dcd2a840513
dobidobz <dobidobz@gmail.com>
2016-05-16 14:42:44 +0800

    [Admin] Client Subscriptions API Docs

--- api/app/Http/Controllers/v1/admin/ClientsController.php
+++ api/app/Http/Controllers/v1/admin/ClientsController.php
@@ -67,21 +67,214 @@ class ClientsController extends ApiController
         return $service->update($request, $id);
     }
 
+    /**
+     * Get all client subscription history
+     *
+     * @SWG\Get(
+     *     path="/admin/client/subscription",
+     *     tags={"Admin - Client Subscription"},
+     *     summary="All Subscription History",
+     *     description="Get all client subscription history",
+     *     @SWG\Response(response="200", description="Success",
+     *         @SWG\Schema(title="response", type="object", required={"success", "message", "data"},
+     *             @SWG\Property(property="success", type="boolean", description="Is success", default="true"),
+     *             @SWG\Property(property="message", type="string", description="Success message", default="Success"),
+     *             @SWG\Property(property="data", type="array", items=@SWG\Property(ref="#/definitions/ClientSubscriptionResponse"))
+     *         )
+     *     ),
+     *     @SWG\Response(response="403", description="Authentication Failed",
+     *         @SWG\Schema(title="response", type="object", required={"success", "message", "messages"},
+     *             @SWG\Property(property="success", type="boolean", description="Is success", default="false"),
+     *             @SWG\Property(property="message", type="string", description="Error message", default="Authentication Failed"),
+     *             @SWG\Property(property="messages", type="array", description="Other messages or instructions for user", items=""),
+     *         )
+     *     ),
+     *     @SWG\Parameter(
+     *         name="X-Token",
+     *         in="header",
+     *         description="X-Token",
+     *         required=true,
+     *         type="string",
+     *         default=""
+     *     ),
+     *     @SWG\Parameter(name="client_id", in="query", description="FILTER by client_id", required=false, type="integer", default=""),
+     *     @SWG\Parameter(name="subscription_id", in="query", description="FILTER by subscription_id", required=false, type="integer", default=""),
+     *     @SWG\Parameter(name="valid_from", in="query", description="FILTER by valid_from (YYYY-MM-DD)", required=false, type="string", format="date", default=""),
+     *     @SWG\Parameter(name="valid_to", in="query", description="FILTER by valid_to (YYYY-MM-DD)", required=false, type="string", format="date", default=""),
+     *     @SWG\Parameter(name="valid_range_from", in="query", description="FILTER by valid_range_from (YYYY-MM-DD) (not exact specific)", required=false, type="string", format="date", default=""),
+     *     @SWG\Parameter(name="valid_range_to", in="query", description="FILTER by valid_range_to (YYYY-MM-DD) (not exact specific)", required=false, type="string", format="date", default=""),
+     *     @SWG\Parameter(name="sort_by", in="query", description="Order by according to: created_at (default)", required=false, type="string", default=""),
+     *     @SWG\Parameter(name="sort_dir", in="query", description="Order by direction: asc => ascending, desc (default) => descending", required=false, type="string", default=""),
+     *     @SWG\Parameter(name="per_page", in="query", description="for pagination, number of items to return per page", required=false, type="integer", default=""),
+     *     @SWG\Parameter(name="page", in="query", description="for pagination, show items belonging to page", required=false, type="integer", default=""),
+     *     @SWG\Parameter(name="max_pagination_links", in="query", description="for pagination, maximum number of pages", required=false, type="integer", default=""),
+     * )
+     *
+     * @param ClientServices $service
+     *
+     * @return mixed
+     */
     public function getSubscriptionHistory(ClientServices $service)
     {
         return $service->getSubscriptionHistory($this->request);
     }
 
+    /**
+     * Get all client's current subscription
+     *
+     * @SWG\Get(
+     *     path="/admin/client/subscription/current",
+     *     tags={"Admin - Client Subscription"},
+     *     summary="All Current Subscription",
+     *     description="Get all client's current subscription",
+     *     @SWG\Response(response="200", description="Success",
+     *         @SWG\Schema(title="response", type="object", required={"success", "message", "data"},
+     *             @SWG\Property(property="success", type="boolean", description="Is success", default="true"),
+     *             @SWG\Property(property="message", type="string", description="Success message", default="Success"),
+     *             @SWG\Property(property="data", description="Data", ref="#/definitions/ClientSubscriptionResponse")
+     *         )
+     *     ),
+     *     @SWG\Response(response="403", description="Authentication Failed",
+     *         @SWG\Schema(title="response", type="object", required={"success", "message", "messages"},
+     *             @SWG\Property(property="success", type="boolean", description="Is success", default="false"),
+     *             @SWG\Property(property="message", type="string", description="Error message", default="Authentication Failed"),
+     *             @SWG\Property(property="messages", type="array", description="Other messages or instructions for user", items=""),
+     *         )
+     *     ),
+     *     @SWG\Parameter(
+     *         name="X-Token",
+     *         in="header",
+     *         description="X-Token",
+     *         required=true,
+     *         type="string",
+     *         default=""
+     *     ),
+     *     @SWG\Parameter(name="client_id", in="query", description="FILTER by client_id", required=false, type="integer", default=""),
+     *     @SWG\Parameter(name="subscription_id", in="query", description="FILTER by subscription_id", required=false, type="integer", default=""),
+     *     @SWG\Parameter(name="valid_from", in="query", description="FILTER by valid_from (YYYY-MM-DD)", required=false, type="string", format="date", default=""),
+     *     @SWG\Parameter(name="valid_to", in="query", description="FILTER by valid_to (YYYY-MM-DD)", required=false, type="string", format="date", default=""),
+     *     @SWG\Parameter(name="valid_range_from", in="query", description="FILTER by valid_range_from (YYYY-MM-DD) (not exact specific)", required=false, type="string", format="date", default=""),
+     *     @SWG\Parameter(name="valid_range_to", in="query", description="FILTER by valid_range_to (YYYY-MM-DD) (not exact specific)", required=false, type="string", format="date", default=""),
+     *     @SWG\Parameter(name="sort_by", in="query", description="Order by according to: created_at (default)", required=false, type="string", default=""),
+     *     @SWG\Parameter(name="sort_dir", in="query", description="Order by direction: asc => ascending, desc (default) => descending", required=false, type="string", default=""),
+     *     @SWG\Parameter(name="per_page", in="query", description="for pagination, number of items to return per page", required=false, type="integer", default=""),
+     *     @SWG\Parameter(name="page", in="query", description="for pagination, show items belonging to page", required=false, type="integer", default=""),
+     *     @SWG\Parameter(name="max_pagination_links", in="query", description="for pagination, maximum number of pages", required=false, type="integer", default=""),
+     * )
+     *
+     * @param ClientServices $service
+     *
+     * @return mixed
+     */
     public function getSubscription(ClientServices $service)
     {
         return $service->getSubscription($this->request);
     }
 
+    /**
+     * Cancel Client Subscription
+     *
+     * @SWG\Patch(
+     *     path="/admin/client/{client}/subscription/cancel",
+     *     tags={"Admin - Client Subscription"},
+     *     summary="Cancel Subscription",
+     *     description="Cancel Client Subscription",
+     *     @SWG\Response(response="200", description="Success",
+     *         @SWG\Schema(title="response", type="object", required={"success", "message", "data"},
+     *             @SWG\Property(property="success", type="boolean", description="Is success", default="true"),
+     *             @SWG\Property(property="message", type="string", description="Success message", default="Success"),
+     *             @SWG\Property(property="data", description="Data")
+     *         )
+     *     ),
+     *     @SWG\Response(response="403", description="Authentication Failed",
+     *         @SWG\Schema(title="response", type="object", required={"success", "message", "messages"},
+     *             @SWG\Property(property="success", type="boolean", description="Is success", default="false"),
+     *             @SWG\Property(property="message", type="string", description="Error message", default="Authentication Failed"),
+     *             @SWG\Property(property="messages", type="array", description="Other messages or instructions for user", items=""),
+     *         )
+     *     ),
+     *     @SWG\Parameter(
+     *         name="X-Token",
+     *         in="header",
+     *         description="X-Token",
+     *         required=true,
+     *         type="string",
+     *         default=""
+     *     ),
+     *     @SWG\Parameter(
+     *         name="client",
+     *         in="path",
+     *         description="Client ID",
+     *         required=true,
+     *         type="string",
+     *         default="1"
+     *     ),
+     * )
+     *
+     * @param $id
+     * @param ClientServices $service
+     *
+     * @return mixed
+     */
     public function cancelSubscription($id, ClientServices $service)
     {
         return $service->cancelSubscription($id);
     }
 
+    /**
+     * Change Client Subscription
+     *
+     * @SWG\Post(
+     *     path="/admin/client/{client}/subscription",
+     *     tags={"Admin - Client Subscription"},
+     *     summary="Change Subscription",
+     *     description="Change Client Subscription.",
+     *     @SWG\Response(response="200", description="Success",
+     *         @SWG\Schema(title="response", type="object", required={"success", "message", "data"},
+     *             @SWG\Property(property="success", type="boolean", description="Is success", default="true"),
+     *             @SWG\Property(property="message", type="string", description="Success message", default="Success"),
+     *             @SWG\Property(property="data", description="Data", ref="#/definitions/ClientSubscriptionResponse")
+     *         )
+     *     ),
+     *     @SWG\Response(response="403", description="Authentication Failed",
+     *         @SWG\Schema(title="response", type="object", required={"success", "message", "messages"},
+     *             @SWG\Property(property="success", type="boolean", description="Is success", default="false"),
+     *             @SWG\Property(property="message", type="string", description="Error message", default="Authentication Failed"),
+     *             @SWG\Property(property="messages", type="array", description="Other messages or instructions for user", items=""),
+     *         )
+     *     ),
+     *     @SWG\Parameter(
+     *         name="X-Token",
+     *         in="header",
+     *         description="X-Token",
+     *         required=true,
+     *         type="string",
+     *         default=""
+     *     ),
+     *     @SWG\Parameter(
+     *         name="client",
+     *         in="path",
+     *         description="Client ID",
+     *         required=true,
+     *         type="string",
+     *         default="1"
+     *     ),
+     *     @SWG\Parameter(
+     *         name="body",
+     *         in="body",
+     *         description="Subscription",
+     *         required=true,
+     *         type="object",
+     *         @SWG\Schema(title="data", type="object", ref="#/definitions/ClientSubscription"))
+     *     )
+     * )
+     *
+     * @param SubscriptionRequest $request
+     * @param $id
+     * @param ClientServices $service
+     *
+     * @return mixed
+     */
     public function purchaseSubscription(SubscriptionRequest $request, $id, ClientServices $service)
     {
         return $service->purchaseSubscription($request, $id);
