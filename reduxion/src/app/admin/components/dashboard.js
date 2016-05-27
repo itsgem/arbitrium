89c8ac39e76959c95159a25a23a7ad5b37550170
Ilyn Rose Abletes <ilynroseabletes@brenda.local>
2016-05-27 11:30:12 +0800

    #178 Admin Site - Dashboard -> please remove alert message at dashboard

--- reduxion/src/app/admin/components/dashboard.js
+++ reduxion/src/app/admin/components/dashboard.js
@@ -24,12 +24,6 @@ class Dashboard extends React.Component {
           <AdminHeader />
           <div className="mdl-layout__content">
             <div id="top" className="mdl-grid client-list">
-            <div className="alert alert-warning">
-              <i className="material-icons"></i>
-              <div className="content">
-                Please fill in the following required fields below.
-              </div>
-            </div>
             {this.props.children}
             </div>
           </div>
