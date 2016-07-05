135083a4cd11c5183c7d06db09d869786f7063bb
dobidobz <dobidobz@gmail.com>
2016-07-05 18:02:36 +0800

    PR review: declare repeating lines as variable

--- api/app/Http/Requests/v1/Admin/AdminUserRequest.php
+++ api/app/Http/Requests/v1/Admin/AdminUserRequest.php
@@ -64,7 +64,8 @@ class AdminUserRequest extends NrbRequest
         else
         {
             // [Core-API] Check if username already taken
-            $admin_id = ((int) last($this->segments()) != 0) ? last($this->segments()) : get_logged_in_admin_id();
+            $admin_id = last($this->segments());
+            $admin_id = ((int) $admin_id != 0) ? $admin_id : get_logged_in_admin_id();
             $username = Admin::findOrFail($admin_id)->user->username;
 
             if ($this->get('username') && $this->get('username') != $username)
