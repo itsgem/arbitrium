4b3426edcde24d0c3f95ec43c8da740fb59c607c
irrailyn <ilyn@idearobin.com>
2016-07-25 10:53:00 +0800

    #288 Client Site - remove Account and Setting then add Profile at list

--- reduxion/src/app/client/components/header.js
+++ reduxion/src/app/client/components/header.js
@@ -19,7 +19,7 @@ class ClientHeader extends React.Component {
           <div className="mdl-layout-spacer"></div>
           <div className="wrapper">
           <nav className="mdl-navigation">
-            <div className="container">
+            {/*<div className="container">
               <a id="demo-menu-account"
                 className="mdl-button mdl-js-button">{tr.t('CLIENT_HEADER.LABEL.ACCOUNT')}</a>
               <ul className="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" htmlFor="demo-menu-account">
@@ -27,13 +27,13 @@ class ClientHeader extends React.Component {
                 <li className="mdl-menu__item"><Link to="/i/client/profile/change_password">{tr.t('CLIENT_HEADER.CHANGE_PW')}</Link></li>
                 <li className="mdl-menu__item"><Link to="/i/client/profile/change_email">{tr.t('CLIENT_HEADER.CHANGE_EMAIL_ADD')}</Link></li>
               </ul>
-            </div>
+            </div>*/}
             <div className="container">
               <button id="demo_menu-lower-right" className="mdl-button mdl-js-button mdl-button--icon" data-upgraded=",MaterialButton">
                 <i className="material-icons">person</i>
               </button>
               <ul className="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" htmlFor="demo_menu-lower-right">
-                <li className="mdl-menu__item"><Link className="mdl-navigation__link " to ="/i"><i className="material-icons mdl-list__item-icon">settings</i>{tr.t('CLIENT_HEADER.SETTINGS')}</Link></li>
+                <li className="mdl-menu__item"><Link className="mdl-navigation__link " to ="/i/client/profile"><i className="material-icons mdl-list__item-icon">person</i>{tr.t('CLIENT_HEADER.PROFILE')}</Link></li>
                 <li className="mdl-menu__item"><a className="mdl-navigation__link " href ="/i/logout"><i className="material-icons mdl-list__item-icon">exit_to_app</i>{tr.t('CLIENT_HEADER.LOGOUT')}</a></li>
               </ul>
             </div>
