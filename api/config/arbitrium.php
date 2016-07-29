5672be6789e891364441e8c7a26c784fb1d445c8
gelgamish <robin@gabrielles-MacBook-Air.local>
2016-07-29 15:46:45 +0800

    fix alignment

--- api/config/arbitrium.php
+++ api/config/arbitrium.php
@@ -18,29 +18,29 @@ return [
             'password'      => env('ARBITRIUM_PASSWORD'),
         ],
         'endpoints'     => [
-            'authenticate'              => ['method' => 'post',   'path' => 'oauth/token'],
-            'create_user'               => ['method' => 'post',   'path' => 'users'],
-            'update_user'               => ['method' => 'put',    'path' => 'users/:id'],
-            'delete_user'               => ['method' => 'delete', 'path' => 'users/:id'],
-            'check_username'            => ['method' => 'get',    'path' => 'users/check/:username'],
+            'authenticate'                  => ['method' => 'post',   'path' => 'oauth/token'],
+            'create_user'                   => ['method' => 'post',   'path' => 'users'],
+            'update_user'                   => ['method' => 'put',    'path' => 'users/:id'],
+            'delete_user'                   => ['method' => 'delete', 'path' => 'users/:id'],
+            'check_username'                => ['method' => 'get',    'path' => 'users/check/:username'],
 
-            'list_api_keys'             => ['method' => 'get',    'path' => 'apiKeys'],
-            'show_api_key'              => ['method' => 'get',    'path' => 'apiKeys/:id'],
-            'create_api_key'            => ['method' => 'post',   'path' => 'apiKeys'],
-            'update_api_key'            => ['method' => 'put',    'path' => 'apiKeys/:id'],
-            'delete_api_key'            => ['method' => 'delete', 'path' => 'apiKeys/:id'],
-            'activate_api_key'          => ['method' => 'patch',  'path' => 'apiKeys/:id/activate'],
+            'list_api_keys'                 => ['method' => 'get',    'path' => 'apiKeys'],
+            'show_api_key'                  => ['method' => 'get',    'path' => 'apiKeys/:id'],
+            'create_api_key'                => ['method' => 'post',   'path' => 'apiKeys'],
+            'update_api_key'                => ['method' => 'put',    'path' => 'apiKeys/:id'],
+            'delete_api_key'                => ['method' => 'delete', 'path' => 'apiKeys/:id'],
+            'activate_api_key'              => ['method' => 'patch',  'path' => 'apiKeys/:id/activate'],
 
-            'list_api_key_permissions'  => ['method' => 'get',    'path' => 'permission'],
+            'list_api_key_permissions'      => ['method' => 'get',    'path' => 'permission'],
 
-            'list_api_logs'             => ['method' => 'get',    'path' => 'apilogs'],
-            'show_api_log'              => ['method' => 'get',    'path' => 'apilogs/:id'],
+            'list_api_logs'                 => ['method' => 'get',    'path' => 'apilogs'],
+            'show_api_log'                  => ['method' => 'get',    'path' => 'apilogs/:id'],
 
-            'show_api_reports'               => ['method' => 'get',    'path' => 'reports'],
-            'show_api_reports_by_client'        => ['method' => 'get',    'path' => 'reports/client/:date'],
-            'show_api_reports_detail'        => ['method' => 'get',    'path' => 'reports/:id'],
+            'show_api_reports'              => ['method' => 'get',    'path' => 'reports'],
+            'show_api_reports_by_client'    => ['method' => 'get',    'path' => 'reports/client/:date'],
+            'show_api_reports_detail'       => ['method' => 'get',    'path' => 'reports/:id'],
 
-            'subscribe'                 => ['method' => 'post',   'path' => 'subscribe'],
+            'subscribe'                     => ['method' => 'post',   'path' => 'subscribe'],
         ]
     ],
 
