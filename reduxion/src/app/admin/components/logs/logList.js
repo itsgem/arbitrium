8e864104eda19e58c9f3178022a22219ee1f9a46
irrailyn <ilyn@idearobin.com>
2016-08-02 14:25:20 +0800

    API Logs - Updated date created field for CSV Download

--- reduxion/src/app/admin/components/logs/logList.js
+++ reduxion/src/app/admin/components/logs/logList.js
@@ -147,7 +147,7 @@ class LogList extends React.Component {
     let perPage = 10;
     let logList = {last_page: 1};
     let log = {};
-    let fields = ['created', 'ipaddress', 'method', 'status_code', 'url', 'parameter'];
+    let fields = ['created_at', 'ipaddress', 'method', 'status_code', 'url', 'parameter'];
     let estateNameCsv = '';
     let datacsv = '';
 
